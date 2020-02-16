e6a5e1d431abe79fe3e63bdec9589aff5ee2454e
john-medeiros <john.sql@gmail.com>
2020-02-15 21:58:17 -0300

    aa

--- detail.py
+++ detail.py
@@ -20,23 +20,128 @@ def init():
     fh.setFormatter(formatter)
     logger.setLevel(logging.INFO)
     logger.addHandler(fh)
+    logging.info('PID: ' + str(os.getpid()))
 
 class Anuncio:
     
     def __init__(self,id,url,titulo=None,preco=None,ano=None,km=None,cor=None,cambio=None,portas=None,acessorio=None,info_veiculo=None):
-        self.id = str(id)
-        self.url = str(url)
-        self.titulo = str(titulo)
-        self.preco = str(preco)
-        self.ano = str(ano)
-        self.km = str(km)
-        self.cor = str(cor)
-        self.cambio = str(cambio)
-        self.portas = str(portas)  
-        self.acessorio = str(acessorio)
-        self.info_veiculo = str(info_veiculo)
-        self.data_inicio =  datetime.datetime.now()
-        self.data_fim = None
+        self.__id = str(id)
+        self.__url = str(url)
+        self.__titulo = str(titulo)
+        self.__preco = str(preco)
+        self.__ano = str(ano)
+        self.__km = str(km)
+        self.__cor = str(cor)
+        self.__cambio = str(cambio)
+        self.__portas = str(portas)  
+        self.__acessorio = str(acessorio)
+        self.__info_veiculo = str(info_veiculo)
+        self.__data_inicio =  datetime.datetime.now()
+        self.__data_fim = None
+    
+    @property
+    def id(self):
+        return self.__id
+
+    @id.setter
+    def id(self, value):
+        self.__id = str(value)
+
+    @property
+    def url(self):
+        return self.__url
+
+    @url.setter
+    def url(self, value):
+        self.__url = str(value)
+
+    @property
+    def titulo(self):
+        return self.__titulo
+
+    @titulo.setter
+    def titulo(self, value):
+        self.__titulo = str(value)
+
+    @property
+    def preco(self):
+        return self.__preco
+
+    @preco.setter
+    def preco(self, value):
+        self.__preco = str(value)        
+
+    @property
+    def ano(self):
+        return self.__ano
+
+    @ano.setter
+    def ano(self, value):
+        self.__ano = str(value)          
+
+    @property
+    def km(self):
+        return self.__km
+
+    @km.setter
+    def km(self, value):
+        self.__km = str(value)  
+
+    @property
+    def cor(self):
+        return self.__cor
+
+    @cor.setter
+    def cor(self, value):
+        self.__cor = str(value)   
+
+    @property
+    def cambio(self):
+        return self.__cambio
+
+    @cambio.setter
+    def cambio(self, value):
+        self.__cambio = str(value)
+
+    @property
+    def portas(self):
+        return self.__portas
+
+    @portas.setter
+    def portas(self, value):
+        self.__portas = str(value)
+
+    @property
+    def acessorio(self):
+        return self.__acessorio
+
+    @acessorio.setter
+    def acessorio(self, value):
+        self.__acessorio = str(value) 
+
+    @property
+    def info_veiculo(self):
+        return self.__info_veiculo
+
+    @info_veiculo.setter
+    def info_veiculo(self, value):
+        self.__info_veiculo = str(value)
+
+    @property
+    def data_inicio(self):
+        return self.__data_inicio
+
+    @data_inicio.setter
+    def data_inicio(self, value):
+        self.__data_inicio = value
+
+    @property
+    def data_fim(self):
+        return self.__data_fim
+
+    @data_fim.setter
+    def data_fim(self, value):
+        self.__data_fim = value
 
     def valida_conteudo_obtido(self):
         if self.titulo is None:
@@ -178,11 +283,18 @@ def obter_detalhe_anuncio(id,url):
         return anuncio
 
 def main():
-    init()
-    anuncio_retornado = obter_detalhe_anuncio("ac28542875","https://www.icarros.com.br/comprar/sao-paulo-sp/chevrolet/onix/2019/d28510029")
-    anuncio_retornado.valida_conteudo_obtido()
-    anuncio_retornado.Salva_CSV("d:/teste.csv")
-
+    try:
+        init()
+        anuncio_retornado = obter_detalhe_anuncio("ac28542875","https://www.icarros.com.br/comprar/sao-paulo-sp/chevrolet/onix/2019/d28510029")
+        anuncio_retornado.valida_conteudo_obtido()
+        anuncio_retornado.Salva_CSV("d:/teste.csv")
+    except Exception as e:
+        #print(e)
+        logging.error('Ocorreu um erro ao processar o Id: ' + id)
+        logging.exception(e)
+        raise
+    finally:
+        logging.info('Processamento finalizado.')
 
 if __name__ == '__main__':
     main()
--- main2.py
+++ main2.py
@@ -7,11 +7,126 @@ import logging.handlers
 import datetime
 import os.path
 
+class Anuncio:
+    
+    def __init__(self,id,url,titulo=None,preco=None,ano=None,km=None,cor=None,cambio=None,portas=None,acessorio=None,info_veiculo=None):
+        self.__id = str(id)
+        self.__url = str(url)
+        self.__titulo = str(titulo)
+        self.__preco = str(preco)
+        self.__ano = str(ano)
+        self.__km = str(km)
+        self.__cor = str(cor)
+        self.__cambio = str(cambio)
+        self.__portas = str(portas)  
+        self.__acessorio = str(acessorio)
+        self.__info_veiculo = str(info_veiculo)
+        self.__data_inicio =  datetime.datetime.now()
+        self.__data_fim = None
+    
+    @property
+    def id(self):
+        return self.__id
 
+    @id.setter
+    def id(self, value):
+        self.__id = str(value)
 
-def main():
+    @property
+    def url(self):
+        return self.__url
 
+    @url.setter
+    def url(self, value):
+        self.__url = str(value)
 
+    @property
+    def titulo(self):
+        return self.__titulo
 
-if __name__ == '__main__':
-    main()
+    @titulo.setter
+    def titulo(self, value):
+        self.__titulo = str(value)
+
+    @property
+    def preco(self):
+        return self.__preco
+
+    @preco.setter
+    def preco(self, value):
+        self.__preco = str(value)        
+
+    @property
+    def ano(self):
+        return self.__ano
+
+    @ano.setter
+    def ano(self, value):
+        self.__ano = str(value)          
+
+    @property
+    def km(self):
+        return self.__km
+
+    @km.setter
+    def km(self, value):
+        self.__km = str(value)  
+
+    @property
+    def cor(self):
+        return self.__cor
+
+    @cor.setter
+    def cor(self, value):
+        self.__cor = str(value)   
+
+    @property
+    def cambio(self):
+        return self.__cambio
+
+    @cambio.setter
+    def cambio(self, value):
+        self.__cambio = str(value)
+
+    @property
+    def portas(self):
+        return self.__portas
+
+    @portas.setter
+    def portas(self, value):
+        self.__portas = str(value)
+
+    @property
+    def acessorio(self):
+        return self.__acessorio
+
+    @acessorio.setter
+    def acessorio(self, value):
+        self.__acessorio = str(value) 
+
+    @property
+    def info_veiculo(self):
+        return self.__info_veiculo
+
+    @info_veiculo.setter
+    def info_veiculo(self, value):
+        self.__info_veiculo = str(value)
+
+    @property
+    def data_inicio(self):
+        return self.__data_inicio
+
+    @data_inicio.setter
+    def data_inicio(self, value):
+        self.__data_inicio = value
+
+    @property
+    def data_fim(self):
+        return self.__data_fim
+
+    @data_fim.setter
+    def data_fim(self, value):
+        self.__data_fim = value
+
+a = Anuncio('dasd','assda')        
+a.acessorio='dsfs'
